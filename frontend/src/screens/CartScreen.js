96c9c0fb4d2a6d7bcff436411c027a25924e42e1
sachin singh <sachinit254@gmail.com>
2021-03-11 16:50:13 -0800

    implemented delete button

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addToCart } from "../actions/cartActions";
+import { addToCart, removeFromCart } from "../actions/cartActions";
 import { Link } from 'react-router-dom';
 import MessageBox from '../components/MessageBox';
 
@@ -18,7 +18,7 @@ export default function CartScreen(props) {
     }
   }, [dispatch, productId, qty]);
   const removeFromCartHandler = (id) => {
-    // delete action
+    dispatch(removeFromCart(id));
   };
 
   const checkoutHandler = () => {
