3bff65ad2f6e6835690be8af272626d043029ce4
sachin singh <sachinit254@gmail.com>
2021-03-11 16:08:39 -0800

    created add to cart red badge

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-
 import Product from "../components/Product";
 import MessageBox from "../components/MessageBox";
 import LoadingBox from "../components/LoadingBox";
