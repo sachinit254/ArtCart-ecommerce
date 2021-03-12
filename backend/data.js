1ab844560de7f5ae3eec02d409ff52d024f3c01d
sachin singh <sachinit254@gmail.com>
2021-03-12 12:50:51 -0800

    sample users in mongodb

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'sachin',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'abhi',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    },
+  ],
     products: [
       {
         _id: "1",
