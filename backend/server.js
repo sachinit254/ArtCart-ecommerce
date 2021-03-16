012b35967820e3f0a0cb5513f4615ac3a0ae4af8
sachin singh <sachinit254@gmail.com>
2021-03-16 15:29:10 -0700

    create place order api and place order database schema

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
+import orderRouter from "./routers/orderRouters.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/ArtCart", {
 
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
+app.use('/api/orders', orderRouter);
 app.get("/", (req, res) => {
   req.setEncoding("Server is ready");
 });
